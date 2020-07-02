b0dddc3680dd54f7695a234879917122cfd4a3c1
Kumar, Abhijit SBOBNG-ITD RLS <Abhijit.A.Kumar@shell.com>
2020-07-03 03:46:37 +0530

    Added the rule handler

---
+++ OrderProcessor.Business/Interfaces/IRuleHandler.cs
@@ -0,0 +1,10 @@
+﻿using OrderProcessor.DTO;
+
+namespace OrderProcessor.Business.Interfaces
+{
+    public interface IRuleHandler
+    {
+        bool IsHandle(OrderItem orderItem);
+        OrderItem ProcessOrderItem(Order order, OrderItem orderItem);
+    }
+}
