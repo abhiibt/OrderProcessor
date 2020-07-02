79cb0a0f78b798ddb4579f67d167505b158f5020
Kumar, Abhijit SBOBNG-ITD RLS <Abhijit.A.Kumar@shell.com>
2020-07-03 03:21:02 +0530

    Added all required projects

---
+++ OrderProcessor.Business.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace OrderProcessor.Business.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ OrderProcessor.DTO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrderProcessor.DTO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
