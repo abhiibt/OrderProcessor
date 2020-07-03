2b982cde545c45cc7602fcbd71bd2e198c39be91
Kumar, Abhijit SBOBNG-ITD RLS <Abhijit.A.Kumar@shell.com>
2020-07-03 11:56:50 +0530

    Added membershipUpgradehandler with unit test

---
+++ OrderProcessor.Business.Tests/Concrete/RuleHandlers/MembershipUpgradeHandlerTests.cs
@@ -0,0 +1,70 @@
+﻿using Moq;
+using OrderProcessor.Business.Concrete.RuleHandlers;
+using OrderProcessor.Business.Interfaces;
+using OrderProcessor.Business.Tests.TestData;
+using OrderProcessor.DTO;
+using System.Linq;
+using Xunit;
+
+namespace OrderProcessor.Business.Tests.Concrete.RuleHandlers
+{
+    public class MembershipUpgradeHandlerTests
+    {
+        private Mock<INotificationProvider> _mockNotificationProvider;
+
+        public MembershipUpgradeHandlerTests()
+        {
+            _mockNotificationProvider = new Mock<INotificationProvider>();
+        }
+
+        [Fact]
+        public void IsHandleTrue()
+        {
+            var unitUnderTest = new MembershipUpgradeHandler(_mockNotificationProvider.Object);
+            var result = unitUnderTest.IsHandle(OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.UpgradeMembership).FirstOrDefault().OrderItems.FirstOrDefault());
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsHandleFalse()
+        {
+            var unitUnderTest = new MembershipUpgradeHandler(_mockNotificationProvider.Object);
+            var result = unitUnderTest.IsHandle(OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.SkiVideo).FirstOrDefault().OrderItems.FirstOrDefault());
+            Assert.False(result);
+        }
+        [Fact]
+        public void ProcessOrderItemIsSuccessUpgradeMembership()
+        {
+            var unitUnderTest = new MembershipUpgradeHandler(_mockNotificationProvider.Object);
+            var result = unitUnderTest.ProcessOrderItem(OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.UpgradeMembership).FirstOrDefault(),
+                OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.ActivateMembership).FirstOrDefault().OrderItems.FirstOrDefault());
+            var steps = result.OrderProcessSteps.Split(",");
+            Assert.NotNull(result.OrderProcessSteps);
+            Assert.Contains($"Upgrade Membership to {OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.UpgradeMembership).FirstOrDefault().OrderItems.FirstOrDefault().SKUId} for customerId {OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.UpgradeMembership).FirstOrDefault().Customer.CustomerId}", steps[0]);
+        }
+
+        [Fact]
+        public void ProcessOrderItemIsSuccessActivateMembership()
+        {
+            var unitUnderTest = new MembershipUpgradeHandler(_mockNotificationProvider.Object);
+            var result = unitUnderTest.ProcessOrderItem(OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.UpgradeMembership).FirstOrDefault(),
+                OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.ActivateMembership).FirstOrDefault().OrderItems.FirstOrDefault());
+            var steps = result.OrderProcessSteps.Split(",");
+            Assert.NotNull(result.OrderProcessSteps);
+            Assert.Contains($"Activate Upgraded Membership - {OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.UpgradeMembership).FirstOrDefault().OrderItems.FirstOrDefault().SKUId} for customerId {OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.UpgradeMembership).FirstOrDefault().Customer.CustomerId}", steps[1]);
+        }
+
+        [Fact]
+        public void ProcessOrderItemIsSuccessNotification()
+        {
+            _mockNotificationProvider.Setup(notification => notification.SendNotification(It.IsAny<Order>(), It.IsAny<string>()))
+               .Returns($"Send email to {OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.UpgradeMembership).FirstOrDefault().Customer.Email} for Activate new membership with skuId - {OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.ActivateMembership).FirstOrDefault().OrderItems.FirstOrDefault().SKUId} and details");
+            var unitUnderTest = new MembershipUpgradeHandler(_mockNotificationProvider.Object);
+            var result = unitUnderTest.ProcessOrderItem(OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.UpgradeMembership).FirstOrDefault(),
+                OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.ActivateMembership).FirstOrDefault().OrderItems.FirstOrDefault());
+            var steps = result.OrderProcessSteps.Split(",");
+            Assert.NotNull(result.OrderProcessSteps);
+            Assert.Contains($"Activate new membership with skuId - {OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.UpgradeMembership).FirstOrDefault().OrderItems.FirstOrDefault().SKUId} and details", steps[2]);
+        }
+    }
+}
---
+++ OrderProcessor.Business/Concrete/RuleHandlers/MembershipUpgradeHandler.cs
@@ -0,0 +1,49 @@
+﻿using OrderProcessor.Business.Interfaces;
+using OrderProcessor.DTO.Common;
+using OrderProcessor.DTO;
+
+namespace OrderProcessor.Business.Concrete.RuleHandlers
+{
+    public class MembershipUpgradeHandler : IRuleHandler
+    {
+        private readonly INotificationProvider _notificationProvider;
+        public MembershipUpgradeHandler(INotificationProvider notificationProvider)
+        {
+            _notificationProvider = notificationProvider;
+        }
+
+        /// <summary>
+        /// This handler will execute only when membership is requested to upgrade is brought
+        /// </summary>
+        /// <param name="orderItem"></param>
+        /// <returns></returns>
+        public bool IsHandle(OrderItem orderItem)
+        {
+            return orderItem.Product == Product.UpgradeMembership;
+        }
+
+        /// <summary>
+        /// handle upgrade membership 
+        /// </summary>
+        /// <param name="order"></param>
+        /// <param name="orderItem"></param>
+        /// <returns></returns>
+        public OrderItem ProcessOrderItem(Order order, OrderItem orderItem)
+        {
+            //Activate the requested membership 
+            var upgradeMembership = $"Upgrade Membership to {orderItem.SKUId} for customerId {order.Customer.CustomerId}";
+            //set the step
+            orderItem.OrderProcessSteps = string.IsNullOrEmpty(orderItem.OrderProcessSteps) ? upgradeMembership : $"{orderItem.OrderProcessSteps},{upgradeMembership}";
+
+            var activateUpgradedMembership = $"Activate Upgraded Membership - {orderItem.SKUId} for customerId {order.Customer.CustomerId}";
+            //set the step
+            orderItem.OrderProcessSteps = $"{orderItem.OrderProcessSteps},{activateUpgradedMembership}";
+            //We can use json string for sending msg in notification. We need to send email to customer with upgrade details
+            var notification = _notificationProvider.SendNotification(order, $"upgrade membership with skuId - {orderItem.SKUId} and details");
+            //set the step
+            orderItem.OrderProcessSteps = $"{orderItem.OrderProcessSteps},{notification}";
+
+            return orderItem;
+        }
+    }
+}
