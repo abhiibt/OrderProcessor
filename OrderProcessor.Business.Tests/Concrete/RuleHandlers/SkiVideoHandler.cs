91584e3db0c9a2370323e479e13261eac4400c05
Kumar, Abhijit SBOBNG-ITD RLS <Abhijit.A.Kumar@shell.com>
2020-07-03 12:16:02 +0530

    Added skiVideo handler with unit tests

---
+++ OrderProcessor.Business.Tests/Concrete/RuleHandlers/SkiVideoHandler.cs
@@ -0,0 +1,52 @@
+﻿using OrderProcessor.Business.Interfaces;
+using OrderProcessor.DTO.Common;
+using OrderProcessor.DTO;
+
+namespace OrderProcessor.Business.Tests.Concrete
+{
+    public class SkiVideoHandler : IRuleHandler
+    {
+        private readonly IShipmentProvider _shipmentProvider;
+        public SkiVideoHandler(IShipmentProvider shipmentProvider)
+        {
+            _shipmentProvider = shipmentProvider;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="orderItem"></param>
+        /// <returns></returns>
+        public bool IsHandle(OrderItem orderItem)
+        {
+            return orderItem.Product == Product.SkiVideo;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="order"></param>
+        /// <param name="orderItem"></param>
+        /// <returns></returns>
+        public OrderItem ProcessOrderItem(Order order, OrderItem orderItem)
+        {
+            //Generate shipping slip for physical product
+            var addingFreeFirstAidVideo = $"Adding free first Aid video";
+            //set the step
+            orderItem.OrderProcessSteps = string.IsNullOrEmpty(orderItem.OrderProcessSteps) ? addingFreeFirstAidVideo : $"{orderItem.OrderProcessSteps},{addingFreeFirstAidVideo}";
+            order.OrderItems.Add(new OrderItem()
+            {
+                OrderItemId = order.OrderItems.Count + 1,
+                OrderProcessSteps = "Adding free first aid video to package",
+                Product = Product.Video,
+                SKUId = (int)Product.Video,
+                Price = 0
+            });
+
+            //Generate shipping slip for physical product
+            var generateShipmentSlip = $"{_shipmentProvider.GeneratePackagingShip(order.Customer)} for shipment of Learning to Ski with free first Aid video";
+            //set the step
+            orderItem.OrderProcessSteps = $"{orderItem.OrderProcessSteps},{generateShipmentSlip}";
+            return orderItem;
+        }
+    }
+}
---
+++ OrderProcessor.Business.Tests/Concrete/RuleHandlers/SkiVideoHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using Moq;
+using OrderProcessor.Business.Interfaces;
+using OrderProcessor.Business.Tests.TestData;
+using OrderProcessor.DTO;
+using System.Linq;
+using Xunit;
+
+namespace OrderProcessor.Business.Tests.Concrete.RuleHandlers
+{
+    public class SkiVideoHandlerTests
+    {
+        private Mock<IShipmentProvider> _mockShipmentProvider;
+
+        public SkiVideoHandlerTests()
+        {
+            _mockShipmentProvider = new Mock<IShipmentProvider>();
+        }
+
+        [Fact]
+        public void IsHandleTrue()
+        {
+            var unitUnderTest = new SkiVideoHandler(_mockShipmentProvider.Object);
+            var result = unitUnderTest.IsHandle(OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.SkiVideo).FirstOrDefault().OrderItems.FirstOrDefault());
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsHandleFalse()
+        {
+            var unitUnderTest = new SkiVideoHandler(_mockShipmentProvider.Object);
+            var result = unitUnderTest.IsHandle(OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.PhysicalProduct).FirstOrDefault().OrderItems.FirstOrDefault());
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ProcessOrderItemIsSuccessForLearningToSkiAddFreeFirstAid()
+        {
+            var unitUnderTest = new SkiVideoHandler(_mockShipmentProvider.Object);
+            var result = unitUnderTest.ProcessOrderItem(OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.SkiVideo).FirstOrDefault(),
+                OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.SkiVideo).FirstOrDefault().OrderItems.FirstOrDefault());
+            var steps = result.OrderProcessSteps.Split(",");
+            Assert.NotNull(result.OrderProcessSteps);
+            Assert.Contains($"Adding free first Aid video", steps[0]);
+        }
+
+        [Fact]
+        public void ProcessOrderItemIsSuccessForLearningToSki()
+        {
+            _mockShipmentProvider.Setup(shipment => shipment.GeneratePackagingShip(It.IsAny<Customer>()))
+               .Returns($"Generate slip for {OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.SkiVideo).FirstOrDefault().Customer.CustomerName} for shipment of Learning to Ski with free first Aid video");
+            var unitUnderTest = new SkiVideoHandler(_mockShipmentProvider.Object);
+            var result = unitUnderTest.ProcessOrderItem(OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.SkiVideo).FirstOrDefault(),
+                OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.SkiVideo).FirstOrDefault().OrderItems.FirstOrDefault());
+            var steps = result.OrderProcessSteps.Split(",");
+            Assert.NotNull(result.OrderProcessSteps);
+            Assert.Contains($"Generate slip for {OrdersRepositoryTestData.OneItemOrderPassProductType(DTO.Common.Product.SkiVideo).FirstOrDefault().Customer.CustomerName} for shipment of Learning to Ski with free first Aid video", steps[1]);
+        }        
+    }
+}
